7f99327d46895d644ecef7ade336c1642fc802fb
xzsx666 <80190093+xzsx666@users.noreply.github.com>
2024-05-31 17:30:27 +0800

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,1 +1,16 @@
+package xzsx;
 
+import java.util.Scanner;
+
+public class LeapYear {
+	 public static void main(String[] args) {
+		 Scanner scan=new Scanner(System.in);
+		 System.out.println("输入一个年份：");	
+		 long year=scan.nextLong();		//用户输入
+		 if(year%4==0&&year%100!=0||year%400==0) 	//是闰年
+			 System.out.println(year+"是闰年");
+		 else 										                //不是闰年
+			 System.out.println(year+"不是闰年");
+	 
+	 }
+}
